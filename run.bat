0ee3e1552450ea0e8f684070ca54ee018390fc67
lohcaiz <loki.prabu66@gmail.com>
2019-11-21 19:45:50 +0530

    updated run file

--- run.bat
+++ run.bat
@@ -1,5 +1,8 @@
-echo "Started API automation, Please wait till it finish"
-newman run Salespro_API_POC.postman_collection.json -e Salespro_env.postman_environment -r htmlextra --reporter-htmlextra-logs --reporter-htmlextra-title "Salespro API Automation Report" --reporter-htmlextra-titleSize 4
+npm install
+
+echo "start automation"
+
+node_modules\newman\bin\newman.js run Salespro_API_POC.postman_collection.json -e Salespro_env.postman_environment
 
 echo "Finished API Automation"
 PAUSE
