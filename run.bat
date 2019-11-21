2248f20ee625a4243c0c55685e517ed9e851cbc7
lohcaiz <loki.prabu66@gmail.com>
2019-11-21 19:51:34 +0530

    updated bat file

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ npm install
 
 echo "start automation"
 
-node_modules\newman\bin\newman.js run Salespro_API_POC.postman_collection.json -e Salespro_env.postman_environment
+node_modules\.bin\newman run Salespro_API_POC.postman_collection.json -e Salespro_env.postman_environment
 
 echo "Finished API Automation"
 PAUSE
